00ea913d40f301f0544b1ab2c10ab8d3f69813cf
Vinicius Silva <vinicius.luiz19@gmail.com>
2020-04-03 00:52:34 -0300

    initial commit

---
+++ src/main/java/br/com/tokenlab/challenge/TokenlabChallengeManagementOfEventsApplication.java
@@ -0,0 +1,13 @@
+package br.com.tokenlab.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenlabChallengeManagementOfEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenlabChallengeManagementOfEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tokenlab/challenge/TokenlabChallengeManagementOfEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tokenlab.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenlabChallengeManagementOfEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
