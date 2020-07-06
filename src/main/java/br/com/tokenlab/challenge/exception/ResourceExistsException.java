1288b4b39be91ccc1f819e68d7f346e97f10c773
Vinicius Silva - Tokenlab <viniciussilva@tokenlab.com.br>
2020-07-06 00:09:40 -0300

    Added common exceptions

---
+++ src/main/java/br/com/tokenlab/challenge/exception/ResourceExistsException.java
@@ -0,0 +1,11 @@
+package br.com.tokenlab.challenge.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ResourceExistsException extends RuntimeException {
+    public ResourceExistsException(String message) {
+        super(message);
+    }
+}
